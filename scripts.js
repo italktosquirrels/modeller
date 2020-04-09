e7acf72e572eee28e4fb5dc08d42a5c8613b1c0d
Alex Gellert <alexgellert1@gmail.com>
2020-04-09 16:16:52 -0400

    radio buttons

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var G_unadjustedFPTotal = 0;
+var technical_Complexity = 0;
 
-function calculateUnadjustedFP(){
+function calculateUnadjustedFP() {
     var input = 0
     var output = 0
     var inquire = 0
@@ -10,35 +11,35 @@ function calculateUnadjustedFP(){
     //I N P U T S
     input = $('#numInput').val()
     inputWeigth = document.querySelector('#inputWeight')
-    inputWeigthValue = inputWeigth.options[inputWeigth.selectedIndex].value; 
+    inputWeigthValue = inputWeigth.options[inputWeigth.selectedIndex].value;
     inputTotal = input * inputWeigthValue
     $('#totalInput').val(inputTotal)
     console.log(inputTotal)
-    //O U T P U T S
+        //O U T P U T S
     output = $('#numOutput').val()
     outputWeigth = document.querySelector('#outputWeight')
-    outputWeigthValue = outputWeigth.options[outputWeigth.selectedIndex].value; 
+    outputWeigthValue = outputWeigth.options[outputWeigth.selectedIndex].value;
     outputTotal = output * outputWeigthValue
     $('#totalOutput').val(outputTotal)
     console.log(outputTotal)
-    //I N Q U I R E S
+        //I N Q U I R E S
     inquire = $('#numInquire').val()
     inquireWeigth = document.querySelector('#inquireWeight')
-    inquireWeigthValue = inquireWeigth.options[inquireWeigth.selectedIndex].value; 
+    inquireWeigthValue = inquireWeigth.options[inquireWeigth.selectedIndex].value;
     inquireTotal = inquire * inquireWeigthValue
     $('#totalInquire').val(inquireTotal)
     console.log(inquireTotal)
-    //F I L E S
+        //F I L E S
     file = $('#numFile').val()
     fileWeigth = document.querySelector('#fileWeight')
-    fileWeigthValue = fileWeigth.options[fileWeigth.selectedIndex].value; 
+    fileWeigthValue = fileWeigth.options[fileWeigth.selectedIndex].value;
     fileTotal = file * fileWeigthValue
     $('#totalFile').val(fileTotal)
     console.log(fileTotal)
-    //I N Q U I R E S
+        //I N Q U I R E S
     interfaces = $('#numInterface').val()
     interfacesWeigth = document.querySelector('#interfacesWeight')
-    interfacesWeigthValue = interfacesWeigth.options[interfacesWeigth.selectedIndex].value; 
+    interfacesWeigthValue = interfacesWeigth.options[interfacesWeigth.selectedIndex].value;
     interfacesTotal = interfaces * interfacesWeigthValue
     $('#totalInterface').val(interfacesTotal)
     console.log(interfacesTotal)
@@ -46,5 +47,31 @@ function calculateUnadjustedFP(){
     UFPtotal = inputTotal + outputTotal + inquireTotal + fileTotal + interfacesTotal
     G_unadjustedFPTotal = UFPtotal
     console.log(G_unadjustedFPTotal)
-     $('#totalUFP').val(G_unadjustedFPTotal)
+    $('#totalUFP').val(G_unadjustedFPTotal)
+}
+
+
+
+$("#technical").submit(function(e) {
+    return false;
+});
+
+function technicalComplexity() {
+    var communicationsRadio;
+    var distributedData;
+    var peformanceCriteria;
+    var heavyHardware;
+    var transactionRates;
+    var dataEntry;
+    var updatingRadio;
+    var computationsRadio;
+    var installation;
+    var operation;
+    var portability;
+    var maintainability;
+    var userEfficiency;
+    var reusability;
+
+    communicationsRadio = $('#communicationsRadio').val()
+
 }
